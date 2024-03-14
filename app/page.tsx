e930c326b206b7d6542b0d9488640753698b752c
Lee Neenan <leeneenan@gmail.com>
2024-03-14 12:41:41 +1100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ export default function Home() {
 
 
   return (
-    <main className="flex mt-20 min-h-screen flex-col items-start  p-24">
-      <div className="font-mono text-sm lg:flex">
-        <p className="text-lg fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <main className="flex min-h-screen flex-col items-start  p-24">
+      <div className="font-mono text-sm">
+        <p className="text-lg fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
           JLL QR Code Authentication by location 
         </p>
       </div>
 
-      <div className="flex flex-col items-center w-full">
+      <div className="flex flex-col items-center w-full mt-20">
         <p className="pb-10 text-slate-400">Scan with phone and enter credentials</p>
         <div className="m-auto">
           <img src={`${url}`} alt="QR Code" />
